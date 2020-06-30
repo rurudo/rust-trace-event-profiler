e873f84482b57bc83bbff15522e52f188dd6e8f5
Yuya Taniguchi <ohs10587.2012@gmail.com>
2020-06-30 23:20:04 +0900

    Hide flow event.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ mod serialize_root;
 mod trace_event;
 
 pub use event_builder::EventBuilder;
-pub use profiler::Profiler;
 pub use serialize_root::TraceEventFormat;
 pub use trace_event::Argument;
 pub use trace_event::TraceEvent;
--- src/profiler.rs
+++ src/profiler.rs
@@ -16,10 +16,9 @@ use crate::TraceEvent;
 use crate::TraceEventFormat;
 
 #[derive(Clone)]
-pub struct Profiler {
-    trace_events: Vec<TraceEvent>,
-    capture_started_time: Instant,
-    current_flow_id: u64,
+pub struct FlowId {
+    id: u64,
+    category: String,
 }
 
 fn current_thread_id() -> u64 {
@@ -29,7 +28,24 @@ fn current_thread_id() -> u64 {
     // thread::current().id().as_u64()
 }
 
-fn metadata<MetadataName: Into<String>, Name: Into<String>>(
+pub trait Eventalize{
+    fn current_timestamp(&self) -> u64;
+    fn begin_duration<Name: Into<String>>(&self, name: Name) -> TraceEvent;
+    fn end_duration(&self) -> TraceEvent;
+
+    fn begin_and_end_duration<Name: Into<String>, Calculation, Return>(
+        &self,
+        name: Name,
+        calculation: Calculation,
+    ) -> (TraceEvent, Return)
+    where
+        Calculation: Fn() -> Return;
+
+    fn begin_flow(&self, flow_id: FlowId) -> Vec<TraceEvent>;
+    fn end_flow(&self, flow_id: FlowId) -> Vec<TraceEvent>;
+}
+
+pub fn metadata<MetadataName: Into<String>, Name: Into<String>>(
     metadata_name: MetadataName,
     name: Name,
 ) -> TraceEvent {
@@ -43,67 +59,51 @@ fn metadata<MetadataName: Into<String>, Name: Into<String>>(
         .unwrap()
 }
 
-impl Profiler {
-    pub fn new() -> Profiler {
-        Profiler {
-            trace_events: Vec::with_capacity(100),
-            capture_started_time: Instant::now(),
-            current_flow_id: 0,
-        }
-    }
+pub fn current_thread_name<Name: Into<String>>(name: Name) -> TraceEvent {
+    metadata("thread_name", name)
+}
 
-    pub fn clear(&mut self) {
-        self.trace_events.clear();
-    }
+pub fn current_process_name<Name: Into<String>>(name: Name) -> TraceEvent {
+    metadata("process_name", name)
+}
 
-    pub fn push(&mut self, event: TraceEvent) {
-        self.trace_events.push(event);
-    }
-
-    pub fn extend(&mut self, profiler: Profiler) {
-        self.trace_events.extend(profiler.trace_events);
-    }
-
+impl Eventalize for Instant {
     fn current_timestamp(&self) -> u64 {
-        let micro_seconds = self.capture_started_time.elapsed().as_micros();
+        let micro_seconds = self.elapsed().as_micros();
         TryFrom::try_from(micro_seconds).unwrap()
     }
 
-    pub fn begin_duration<Name: Into<String>>(&mut self, name: Name) {
-        let event = EventBuilder::default()
+    fn begin_duration<Name: Into<String>>(&self, name: Name) -> TraceEvent {
+        EventBuilder::default()
             .phase(Phase::DurationBegin)
             .name(name)
             .timestamp(self.current_timestamp())
             .process_id(process::id())
             .thread_id(current_thread_id())
             .build_for_trace_event()
-            .unwrap();
-
-        self.push(event);
+            .unwrap()
     }
 
-    pub fn end_duration(&mut self) {
-        let event = EventBuilder::default()
+    fn end_duration(&self) -> TraceEvent {
+        EventBuilder::default()
             .phase(Phase::DurationEnd)
             .timestamp(self.current_timestamp())
             .process_id(process::id())
             .thread_id(current_thread_id())
             .build_for_trace_event()
-            .unwrap();
-
-        self.push(event);
+            .unwrap()
     }
 
-    pub fn begin_and_end_duration<Name: Into<String>, T, Return>(
-        &mut self,
+    fn begin_and_end_duration<Name: Into<String>, Calculation, Return>(
+        &self,
         name: Name,
-        calculation: T,
-    ) -> Return
+        calculation: Calculation,
+    ) -> (TraceEvent, Return)
     where
-        T: Fn() -> Return,
+        Calculation: Fn() -> Return,
     {
         let begin = self.current_timestamp();
-        let u = calculation();
+        let return_value = calculation();
         let end = self.current_timestamp();
 
         let event = EventBuilder::default()
@@ -115,48 +115,34 @@ impl Profiler {
             .thread_id(current_thread_id())
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
 
-        u
+        (event, return_value)
     }
 
-    pub fn current_thread_name<Name: Into<String>>(&mut self, name: Name) {
-        self.push(metadata("thread_name", name));
-    }
-
-    pub fn current_process_name<Name: Into<String>>(&mut self, name: Name) {
-        self.push(metadata("process_name", name));
-    }
-
-    pub fn begin_flow<Name: Into<String>, Category: Into<String>>(
-        &mut self,
-        name: Name,
-        category: Category,
-    ) -> u64 {
-        let id = self.current_flow_id;
-
+    fn begin_flow(&self, flow_id: FlowId) -> Vec<TraceEvent> {
+        let mut events = Vec::with_capacity(3);
         let event = EventBuilder::default()
             .phase(Phase::DurationBegin)
-            .name(name)
+            .name("Begin")
             .timestamp(self.current_timestamp())
             .process_id(process::id())
             .thread_id(current_thread_id())
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
+        events.push(event);
 
         let end = self.current_timestamp();
         let event = EventBuilder::default()
             .phase(Phase::FlowBegin)
-            .name(id.to_string())
+            .name(flow_id.id.to_string())
             .timestamp(end)
             .process_id(process::id())
             .thread_id(current_thread_id())
-            .id(id)
-            .category(category)
+            .id(flow_id.id)
+            .category(flow_id.category)
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
+        events.push(event);
 
         let event = EventBuilder::default()
             .phase(Phase::DurationEnd)
@@ -165,40 +151,35 @@ impl Profiler {
             .thread_id(current_thread_id())
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
+        events.push(event);
 
-        self.current_flow_id += 1;
-        id
+        events
     }
 
-    pub fn end_flow<Name: Into<String>, Category: Into<String>>(
-        &mut self,
-        name: Name,
-        category: Category,
-        id: u64,
-    ) {
+    fn end_flow(&self, flow_id: FlowId) -> Vec<TraceEvent> {
         let begin = self.current_timestamp();
+        let mut events = Vec::with_capacity(3);
         let event = EventBuilder::default()
             .phase(Phase::FlowEnd)
-            .name(id.to_string())
+            .name(flow_id.id.to_string())
             .timestamp(begin)
             .process_id(process::id())
             .thread_id(current_thread_id())
-            .id(id)
-            .category(category)
+            .id(flow_id.id)
+            .category(flow_id.category)
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
+        events.push(event);
 
         let event = EventBuilder::default()
             .phase(Phase::DurationBegin)
-            .name(name)
+            .name("End")
             .timestamp(begin)
             .process_id(process::id())
             .thread_id(current_thread_id())
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
+        events.push(event);
 
         let event = EventBuilder::default()
             .phase(Phase::DurationEnd)
@@ -207,16 +188,18 @@ impl Profiler {
             .thread_id(current_thread_id())
             .build_for_trace_event()
             .unwrap();
-        self.push(event);
-    }
+        events.push(event);
 
-    pub fn save_file<AllocatedPath: AsRef<Path>>(&self, path: AllocatedPath) {
-        let memory = TraceEventFormat::new(self.trace_events.clone());
-        let serialized = serde_json::to_string(&memory).unwrap();
-
-        let mut file = File::create(path).unwrap();
-        let _ = write!(file, "{}", serialized);
+        events
     }
+}
+
+pub fn save_file<AllocatedPath: AsRef<Path>>(path: AllocatedPath, trace_events: Vec<TraceEvent>) {
+    let memory = TraceEventFormat::new(trace_events);
+    let serialized = serde_json::to_string(&memory).unwrap();
+
+    let mut file = File::create(path).unwrap();
+    let _ = write!(file, "{}", serialized);
 }
 
 #[cfg(test)]
@@ -224,108 +207,211 @@ mod tests {
     use super::*;
 
     fn run_dummy_task() {
-        let mut profiler = Profiler::new();
+        let eventalizer = Instant::now();
         for _ in 0..10 {
-            profiler.end_duration();
+            eventalizer.end_duration();
         }
     }
 
     #[test]
     fn simple() {
-        let mut profiler = Profiler::new();
-        profiler.begin_duration("Task A");
-        {
-            profiler.begin_and_end_duration("Task B", run_dummy_task);
-            profiler.begin_and_end_duration("Task C", run_dummy_task);
-        }
-        profiler.end_duration();
-
-        profiler.save_file("simple.json");
-    }
-
-    #[test]
-    fn thread() {
-        use std::sync::{mpsc::channel, Arc, Mutex};
-        let profiler = Arc::new(Mutex::new(Profiler::new()));
-
-        profiler.lock().unwrap().begin_duration("Task A");
-
-        let (sender, receiver) = channel();
-        for name in vec!["Task B", "Task C"] {
-            let sender = sender.clone();
-            let profiler = profiler.clone();
-            thread::spawn(move || {
-                let result = profiler
-                    .lock()
-                    .unwrap()
-                    .begin_and_end_duration(name, run_dummy_task);
-                sender.send(result).unwrap();
-            });
-        }
-
-        let _ = receiver.recv();
-        let _ = receiver.recv();
-
-        {
-            let mut profiler = match profiler.lock() {
-                Ok(p) => p,
-                Err(e) => e.into_inner(),
-            };
-
-            profiler.end_duration();
-            profiler.save_file("thread.json");
-        }
+        let eventalizer = Instant::now();
+        let events = vec![
+            eventalizer.begin_duration("Task A"),
+            match eventalizer.begin_and_end_duration("Task B", run_dummy_task) { (e, _) => e },
+            match eventalizer.begin_and_end_duration("Task C", run_dummy_task) { (e, _) => e },
+            eventalizer.end_duration(),
+        ];
+        save_file("simple.json", events);
     }
 
     #[test]
     fn metadata() {
-        let mut profiler = Profiler::new();
-        profiler.begin_and_end_duration("Task A", || {});
-        // need to rename at the end.
-        profiler.current_process_name("metadata test");
-        profiler.current_thread_name("metadata thread");
-        profiler.save_file("metadata.json");
+        let eventalizer = Instant::now();
+        let events = vec![
+            match eventalizer.begin_and_end_duration("Task A", ||{}) { (e, _) => e },
+            // need to rename at the end.
+            current_process_name("metadata test"),
+            current_thread_name("metadata thread"),
+        ];
+        save_file("metadata.json", events);
+    }
+
+    use std::sync::mpsc::{ channel, Sender, Receiver };
+
+    #[test]
+    fn thread() {
+        let eventalizer = Instant::now();
+        let mut events = vec![
+            eventalizer.begin_duration("Task A"),
+        ];
+
+        let (sender, receiver) = channel();
+        for task_name in vec!["Task B", "Task C"] {
+            let sender = sender.clone();
+            let eventalizer = eventalizer.clone();
+
+            thread::spawn(move || {
+                let events = vec![
+                    match eventalizer.begin_and_end_duration(task_name, run_dummy_task) { (e, _) => e },
+                ];
+                sender.send(events).unwrap();
+            });
+        }
+        events.extend(receiver.recv().unwrap());
+        events.extend(receiver.recv().unwrap());
+
+        events.push(eventalizer.end_duration());
+        save_file("thread.json", events);
     }
 
     #[test]
     fn flow() {
-        use std::sync::{mpsc::channel, Arc, Mutex};
-        let sync_profiler = Arc::new(Mutex::new(Profiler::new()));
-
-        let main_thread_profiler = Profiler::new();
-        let mut mtp = main_thread_profiler;
+        let mut flow_id_counter = 0;
+        let eventalizer = Instant::now();
+        let mut trace_events = Vec::new();
 
         let (sender, receiver) = channel();
-        for task_name in vec!["Task A", "Task B"] {
-            let flow_id = mtp.begin_flow("Spawn", "main_to_spawn");
+
+        for task_name in vec!["Task B", "Task C"] {
+            let link_flow_id = FlowId{ id: flow_id_counter, category: "main_to_spawn".to_string() };
+            flow_id_counter += 1;
+            let unlink_flow_id = FlowId{ id: flow_id_counter, category: "spawn_to_main".to_string() };
+            flow_id_counter += 1;
+
+            let events = eventalizer.begin_flow(link_flow_id.clone());
+            trace_events.extend(events);
 
             let sender = sender.clone();
-            let sync_profiler = sync_profiler.clone();
-            let profiler = mtp.clone();
+            let eventalizer = eventalizer.clone();
 
             thread::spawn(move || {
-                let mut profiler = profiler;
-                profiler.clear();
+                let mut trace_events = Vec::new();
+                trace_events.extend(eventalizer.end_flow(link_flow_id));
 
-                profiler.end_flow("Begin", "main_to_spawn", flow_id);
-                profiler.begin_and_end_duration(task_name, run_dummy_task);
+                trace_events.push(match eventalizer.begin_and_end_duration(task_name, run_dummy_task) { (e, _) => e });
 
-                let flow_id = profiler.begin_flow("End", "spwan_to_main");
-
-                sender.send(flow_id).unwrap();
-                sync_profiler.lock().unwrap().extend(profiler);
+                trace_events.extend(eventalizer.begin_flow(unlink_flow_id.clone()));
+                let package = Package{ trace_events, unlink_flow_id };
+                sender.send(package).unwrap();
             });
         }
 
         for _ in 0..2 {
-            let flow_id =
-                mtp.begin_and_end_duration("Wait for response", || receiver.recv().unwrap());
+            let (begin_and_end_event, package) =
+                eventalizer.begin_and_end_duration("Wait for response", || receiver.recv().unwrap());
+            trace_events.push(begin_and_end_event);
+            trace_events.extend(package.trace_events);
+            trace_events.extend(eventalizer.end_flow(package.unlink_flow_id));
+        }
+        save_file("flow.json", trace_events);
+    }
 
-            mtp.end_flow("End task", "spwan_to_main", flow_id);
+    struct Profiler {
+        eventalizer: Instant,
+        trace_events: Vec<TraceEvent>,
+        sender: Sender<Package>,
+        receiver: Receiver<Package>,
+        flow_id_counter: u64,
+        thread_profiler_counter: u64,
+    }
+
+    impl Profiler {
+        fn new() -> Profiler {
+            let (sender, receiver) = channel();
+            Profiler{
+                eventalizer: Instant::now(),
+                trace_events: Vec::new(),
+                sender,
+                receiver,
+                flow_id_counter: 0,
+                thread_profiler_counter: 0,
+            }
         }
 
-        let sync_profiler = sync_profiler.clone();
-        sync_profiler.lock().unwrap().extend(mtp);
-        sync_profiler.lock().unwrap().save_file("flow.json");
+        fn thread_profiler(&mut self) -> ThreadProfiler {
+            self.thread_profiler_counter += 1;
+
+            let link_flow_id = FlowId{ id: self.flow_id_counter, category: "main_to_spawn".to_string() };
+            self.flow_id_counter += 1;
+            let events = self.eventalizer.begin_flow(link_flow_id.clone());
+            self.trace_events.extend(events);
+
+            let unlink_flow_id = FlowId{ id: self.flow_id_counter, category: "spwan_to_main".to_string() };
+            self.flow_id_counter += 1;
+
+            ThreadProfiler {
+                eventalizer: self.eventalizer.clone(),
+                sender: self.sender.clone(),
+                link_flow_id,
+                unlink_flow_id,
+            }
+        }
+
+        fn wait_for_thread_profiler(&mut self) {
+            for _ in 0..self.thread_profiler_counter {
+                let package = self.receiver.recv().unwrap();
+                self.trace_events.extend(package.trace_events);
+
+                let events = self.eventalizer.end_flow(package.unlink_flow_id);
+                self.trace_events.extend(events);
+            }
+        }
+    }
+
+    struct Package {
+        trace_events: Vec<TraceEvent>,
+        unlink_flow_id: FlowId,
+    }
+
+    struct ThreadProfiler {
+        eventalizer: Instant,
+        sender: Sender<Package>,
+        link_flow_id: FlowId,
+        unlink_flow_id: FlowId,
+    }
+
+    impl ThreadProfiler {
+        fn link_thread(&self) -> Package {
+            Package {
+                trace_events: self.eventalizer.end_flow(self.link_flow_id.clone()),
+                unlink_flow_id: self.unlink_flow_id.clone(),
+            }
+        }
+
+        fn unlink_thread(&self, package: Package) {
+            let events = self.eventalizer.begin_flow(package.unlink_flow_id.clone());
+
+            let mut trace_events = package.trace_events.clone();
+            trace_events.extend(events);
+
+            let package = Package {
+                trace_events,
+                unlink_flow_id: package.unlink_flow_id,
+            };
+            self.sender.send(package).unwrap();
+        }
+    }
+
+    #[test]
+    fn flow2() {
+        let mut profiler = Profiler::new();
+
+        for task_name in vec!["Task B", "Task C"] {
+            let thread_profiler = profiler.thread_profiler();
+
+            thread::spawn(move || {
+                let mut package = thread_profiler.link_thread();
+
+                let (event, _) = thread_profiler.eventalizer.begin_and_end_duration(task_name, || {
+                    run_dummy_task();
+                });
+                package.trace_events.push(event);
+
+                thread_profiler.unlink_thread(package);
+            });
+        }
+        profiler.wait_for_thread_profiler();
+        save_file("flow2.json", profiler.trace_events);
     }
 }
